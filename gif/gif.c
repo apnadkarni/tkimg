978271f5991440786d647b3045328a6caebebdfc
nijtmans <nijtmans@8c8d7abf-59c3-4658-88e4-7cea5a6ab77c>
2006-05-02 07:32:03 +0000

    write GIF;s with real LZW compression
    git-svn-id: https://svn.code.sf.net/p/tkimg/code/trunk@80 8c8d7abf-59c3-4658-88e4-7cea5a6ab77c

--- gif/gif.c
+++ gif/gif.c
@@ -1394,431 +1394,366 @@ int colors;
 }
 
 
-
-/*-----------------------------------------------------------------------
+/*
  *
- * miGIF Compression - mouse and ivo's GIF-compatible compression
+ * GIF Image compression - modified 'compress'
  *
- *          -run length encoding compression routines-
+ * Based on: compress.c - File compression ala IEEE Computer, June 1984.
  *
- * Copyright (C) 1998 Hutchison Avenue Software Corporation
- *               http://www.hasc.com
- *               info@hasc.com
- *
- * Permission to use, copy, modify, and distribute this software and its
- * documentation for any purpose and without fee is hereby granted, provided
- * that the above copyright notice appear in all copies and that both that
- * copyright notice and this permission notice appear in supporting
- * documentation.  This software is provided "AS IS." The Hutchison Avenue 
- * Software Corporation disclaims all warranties, either express or implied, 
- * including but not limited to implied warranties of merchantability and 
- * fitness for a particular purpose, with respect to this code and accompanying
- * documentation. 
- * 
- * The miGIF compression routines do not, strictly speaking, generate files 
- * conforming to the GIF spec, since the image data is not LZW-compressed 
- * (this is the point: in order to avoid transgression of the Unisys patent 
- * on the LZW algorithm.)  However, miGIF generates data streams that any 
- * reasonably sane LZW decompresser will decompress to what we want.
- *
- * miGIF compression uses run length encoding. It compresses horizontal runs 
- * of pixels of the same color. This type of compression gives good results
- * on images with many runs, for example images with lines, text and solid 
- * shapes on a solid-colored background. It gives little or no compression 
- * on images with few runs, for example digital or scanned photos.
- *
- *                               der Mouse
- *                      mouse@rodents.montreal.qc.ca
- *            7D C8 61 52 5D E7 2D 39  4E F1 31 3E E8 B3 27 4B
- *
- *                             ivo@hasc.com
- *
- * The Graphics Interchange Format(c) is the Copyright property of
- * CompuServe Incorporated.  GIF(sm) is a Service Mark property of
- * CompuServe Incorporated.
+ * By Authors:  Spencer W. Thomas       (decvax!harpo!utah-cs!utah-gr!thomas)
+ *              Jim McKie               (decvax!mcvax!jim)
+ *              Steve Davies            (decvax!vax135!petsd!peora!srd)
+ *              Ken Turkowski           (decvax!decwrl!turtlevax!ken)
+ *              James A. Woods          (decvax!ihnp4!ames!jaw)
+ *              Joe Orost               (decvax!vax135!petsd!joe)
  *
  */
 
-static int rl_pixel;
-static int rl_basecode;
-static int rl_count;
-static int rl_table_pixel;
-static int rl_table_max;
-static int just_cleared;
-static int out_bits;
-static int out_bits_init;
-static int out_count;
-static int out_bump;
-static int out_bump_init;
-static int out_clear;
-static int out_clear_init;
-static int max_ocodes;
-static int code_clear;
-static int code_eof;
-static unsigned int obuf;
-static int obits;
-static tkimg_MFile *ofile;
-static unsigned char oblock[MAXCOLORMAPSIZE];
-static int oblen;
+static void output _ANSI_ARGS_((long code));
+static void cl_block _ANSI_ARGS_((void));
+static void cl_hash _ANSI_ARGS_((int hsize));
+static void char_init _ANSI_ARGS_((void));
+static void char_out _ANSI_ARGS_((int c));
+static void flush_char _ANSI_ARGS_((void));
 
-/* Used only when debugging GIF compression code */
-/* #define DEBUGGING_ENVARS */
+static int n_bits;		/* number of bits/code */
+static int maxbits = GIFBITS;	/* user settable max # bits/code */
+static long maxcode;		/* maximum code, given n_bits */
+static long maxmaxcode = (long)1 << GIFBITS;
+				/* should NEVER generate this code */
+#define MAXCODE(n_bits)		(((long) 1 << (n_bits)) - 1)
 
-#ifdef DEBUGGING_ENVARS
+static int		htab[HSIZE];
+static unsigned int	codetab[HSIZE];
+#define HashTabOf(i)	htab[i]
+#define CodeTabOf(i)	codetab[i]
 
-static int verbose_set = 0;
-static int verbose;
-#define VERBOSE (verbose_set?verbose:set_verbose())
+static long hsize = HSIZE;	/* for dynamic table sizing */
 
-static int
-set_verbose(void)
-{
-   verbose = !!getenv("GIF_VERBOSE");
-   verbose_set = 1;
-   return(verbose);
-}
+/*
+ * To save much memory, we overlay the table used by compress() with those
+ * used by decompress().  The tab_prefix table is the same size and type
+ * as the codetab.  The tab_suffix table needs 2**GIFBITS characters.  We
+ * get this from the beginning of htab.  The output stack uses the rest
+ * of htab, and contains characters.  There is plenty of room for any
+ * possible stack (stack used to be 8000 characters).
+ */
 
-#else
+static int free_ent = 0;  /* first unused entry */
 
-#define VERBOSE 0
+/*
+ * block compression parameters -- after all codes are used up,
+ * and compression rate changes, start over.
+ */
+static int clear_flg = 0;
 
-#endif
+static int offset;
+static unsigned int in_count = 1;            /* length of input */
+static unsigned int out_count = 0;           /* # of codes output (for debugging) */
 
+/*
+ * compress stdin to stdout
+ *
+ * Algorithm:  use open addressing double hashing (no chaining) on the
+ * prefix code / next character combination.  We do a variant of Knuth's
+ * algorithm D (vol. 3, sec. 6.4) along with G. Knott's relatively-prime
+ * secondary probe.  Here, the modular division first probe is gives way
+ * to a faster exclusive-or manipulation.  Also do block compression with
+ * an adaptive reset, whereby the code table is cleared when the compression
+ * ratio decreases, but after the table fills.  The variable-length output
+ * codes are re-sized at this point, and a special CLEAR code is generated
+ * for the decompressor.  Late addition:  construct the table according to
+ * file size for noticeable speed improvement on small files.  Please direct
+ * questions about this implementation to ames!jaw.
+ */
 
-static CONST char *
-binformat(v, nbits)
-    unsigned int v;
-    int nbits;
-{
- static char bufs[8][64];
- static int bhand = 0;
- unsigned int bit;
- int bno;
- char *bp;
+static int g_init_bits;
+static tkimg_MFile *g_outfile;
 
- bhand --;
- if (bhand < 0) bhand = (sizeof(bufs)/sizeof(bufs[0]))-1;
- bp = &bufs[bhand][0];
- for (bno=nbits-1,bit=1<<bno;bno>=0;bno--,bit>>=1)
-  { *bp++ = (v & bit) ? '1' : '0';
-    if (((bno&3) == 0) && (bno != 0)) *bp++ = '.';
-  }
- *bp = '\0';
- return(&bufs[bhand][0]);
-}
+static int ClearCode;
+static int EOFCode;
 
-static void
-write_block()
-{
-    int i;
-    unsigned char c;
-
-    if (VERBOSE) {
-        printf("write_block %d:",oblen);
-	for (i=0;i<oblen;i++) printf(" %02x",oblock[i]);
-	printf("\n");
-    }
-    c = oblen;
-    tkimg_Write(ofile, (CONST char *) &c, 1);
-    tkimg_Write(ofile, &oblock[0], oblen);
-    oblen = 0;
-}
-
-static void
-block_out(c)
-    unsigned char c;
-{
- if (VERBOSE) printf("block_out %s\n",binformat(c,8));
- oblock[oblen++] = c;
- if (oblen >= 255) write_block();
-}
-
-static void
-block_flush()
-{
-    if (VERBOSE) printf("block_flush\n");
-    if (oblen > 0) write_block();
-}
-
-static void
-output(val)
-    int val;
-{
- if (VERBOSE) printf("output %s [%s %d %d]\n",binformat(val,out_bits),binformat(obuf,obits),obits,out_bits);
- obuf |= val << obits;
- obits += out_bits;
- while (obits >= 8)
-  { block_out(obuf&0xff);
-    obuf >>= 8;
-    obits -= 8;
-  }
- if (VERBOSE) printf("output leaving [%s %d]\n",binformat(obuf,obits),obits);
-}
-
-static void
-output_flush()
-{
- if (VERBOSE) printf("output_flush\n");
- if (obits > 0) block_out(obuf);
- block_flush();
-}
-
-static void
-did_clear()
-{
- if (VERBOSE) printf("did_clear\n");
- out_bits = out_bits_init;
- out_bump = out_bump_init;
- out_clear = out_clear_init;
- out_count = 0;
- rl_table_max = 0;
- just_cleared = 1;
-}
-
-static void
-output_plain(c)
-    int c;
-{
- if (VERBOSE) printf("output_plain %s\n",binformat(c,out_bits));
- just_cleared = 0;
- output(c);
- out_count ++;
- if (out_count >= out_bump)
-  { out_bits ++;
-    out_bump += 1 << (out_bits - 1);
-  }
- if (out_count >= out_clear)
-  { output(code_clear);
-    did_clear();
-  }
-}
-
-static unsigned int
-isqrt(x)
-    unsigned int x;
-{
- unsigned int r;
- unsigned int v;
-
- if (x < 2) return(x);
- for (v=x,r=1;v;v>>=2,r<<=1) ;
- while (1)
-  { v = ((x / r) + r) / 2;
-    if ((v == r) || (v == r+1)) return(r);
-    r = v;
-  }
-}
-
-static unsigned int
-compute_triangle_count(count, nrepcodes)
-    unsigned int count;
-    unsigned int nrepcodes;
-{
- unsigned int perrep;
- unsigned int cost;
-
- cost = 0;
- perrep = (nrepcodes * (nrepcodes+1)) / 2;
- while (count >= perrep)
-  { cost += nrepcodes;
-    count -= perrep;
-  }
- if (count > 0)
-  { unsigned int n;
-    n = isqrt(count);
-    while ((n*(n+1)) >= 2*count) n --;
-    while ((n*(n+1)) < 2*count) n ++;
-    cost += n;
-  }
- return(cost);
-}
-
-static void
-max_out_clear()
-{
- out_clear = max_ocodes;
-}
-
-static void
-reset_out_clear()
-{
- out_clear = out_clear_init;
- if (out_count >= out_clear)
-  { output(code_clear);
-    did_clear();
-  }
-}
-
-static void
-rl_flush_fromclear(count)
-    int count;
-{
- int n;
-
- if (VERBOSE) printf("rl_flush_fromclear %d\n",count);
- max_out_clear();
- rl_table_pixel = rl_pixel;
- n = 1;
- while (count > 0)
-  { if (n == 1)
-     { rl_table_max = 1;
-       output_plain(rl_pixel);
-       count --;
-     }
-    else if (count >= n)
-     { rl_table_max = n;
-       output_plain(rl_basecode+n-2);
-       count -= n;
-     }
-    else if (count == 1)
-     { rl_table_max ++;
-       output_plain(rl_pixel);
-       count = 0;
-     }
-    else
-     { rl_table_max ++;
-       output_plain(rl_basecode+count-2);
-       count = 0;
-     }
-    if (out_count == 0) n = 1; else n ++;
-  }
- reset_out_clear();
- if (VERBOSE) printf("rl_flush_fromclear leaving table_max=%d\n",rl_table_max);
-}
-
-static void
-rl_flush_clearorrep(count)
-    int count;
-{
- int withclr;
-
- if (VERBOSE) printf("rl_flush_clearorrep %d\n",count);
- withclr = 1 + compute_triangle_count(count,max_ocodes);
- if (withclr < count)
-  { output(code_clear);
-    did_clear();
-    rl_flush_fromclear(count);
-  }
- else
-  { for (;count>0;count--) output_plain(rl_pixel);
-  }
-}
-
-static void
-rl_flush_withtable(count)
-    int count;
-{
- int repmax;
- int repleft;
- int leftover;
-
- if (VERBOSE) printf("rl_flush_withtable %d\n",count);
- repmax = count / rl_table_max;
- leftover = count % rl_table_max;
- repleft = (leftover ? 1 : 0);
- if (out_count+repmax+repleft > max_ocodes)
-  { repmax = max_ocodes - out_count;
-    leftover = count - (repmax * rl_table_max);
-    repleft = 1 + compute_triangle_count(leftover,max_ocodes);
-  }
- if (VERBOSE) printf("rl_flush_withtable repmax=%d leftover=%d repleft=%d\n",repmax,leftover,repleft);
- if (1+compute_triangle_count(count,max_ocodes) < repmax+repleft)
-  { output(code_clear);
-    did_clear();
-    rl_flush_fromclear(count);
-    return;
-  }
- max_out_clear();
- for (;repmax>0;repmax--) output_plain(rl_basecode+rl_table_max-2);
- if (leftover)
-  { if (just_cleared)
-     { rl_flush_fromclear(leftover);
-     }
-    else if (leftover == 1)
-     { output_plain(rl_pixel);
-     }
-    else
-     { output_plain(rl_basecode+leftover-2);
-     }
-  }
- reset_out_clear();
-}
-
-static void
-rl_flush()
-{
- if (VERBOSE) printf("rl_flush [ %d %d\n",rl_count,rl_pixel);
- if (rl_count == 1)
-  { output_plain(rl_pixel);
-    rl_count = 0;
-    if (VERBOSE) printf("rl_flush ]\n");
-    return;
-  }
- if (just_cleared)
-  { rl_flush_fromclear(rl_count);
-  }
- else if ((rl_table_max < 2) || (rl_table_pixel != rl_pixel))
-  { rl_flush_clearorrep(rl_count);
-  }
- else
-  { rl_flush_withtable(rl_count);
-  }
- if (VERBOSE) printf("rl_flush ]\n");
- rl_count = 0;
-}
-
-
-static void
-compress( init_bits, handle, readValue )
+static void compress( init_bits, handle, readValue )
     int init_bits;
     tkimg_MFile *handle;
     ifunptr readValue;
 {
- int c;
+    register long fcode;
+    register long i = 0;
+    register int c;
+    register long ent;
+    register long disp;
+    register long hsize_reg;
+    register int hshift;
 
- ofile = handle;
- obuf = 0;
- obits = 0;
- oblen = 0;
- code_clear = 1 << (init_bits - 1);
- code_eof = code_clear + 1;
- rl_basecode = code_eof + 1;
- out_bump_init = (1 << (init_bits - 1)) - 1;
- /* for images with a lot of runs, making out_clear_init larger will
-    give better compression. */ 
- out_clear_init = (init_bits <= 3) ? 9 : (out_bump_init-1);
-#ifdef DEBUGGING_ENVARS
-  { CONST char *ocienv;
-    ocienv = getenv("GIF_OUT_CLEAR_INIT");
-    if (ocienv)
-     { out_clear_init = atoi(ocienv);
-       if (VERBOSE) printf("[overriding out_clear_init to %d]\n",out_clear_init);
-     }
-  }
+    /*
+     * Set up the globals:  g_init_bits - initial number of bits
+     *                      g_outfile   - pointer to output file
+     */
+    g_init_bits = init_bits;
+    g_outfile = handle;
+
+    /*
+     * Set up the necessary values
+     */
+    offset = 0;
+    out_count = 0;
+    clear_flg = 0;
+    in_count = 1;
+    maxcode = MAXCODE(n_bits = g_init_bits);
+
+    ClearCode = (1 << (init_bits - 1));
+    EOFCode = ClearCode + 1;
+    free_ent = ClearCode + 2;
+
+    char_init();
+
+    ent = readValue();
+
+    hshift = 0;
+    for ( fcode = (long) hsize;  fcode < 65536L; fcode *= 2L )
+        hshift++;
+    hshift = 8 - hshift;                /* set hash code range bound */
+
+    hsize_reg = hsize;
+    cl_hash( (int) hsize_reg);            /* clear hash table */
+
+    output( (long)ClearCode );
+
+#ifdef SIGNED_COMPARE_SLOW
+    while ( (c = readValue() ) != (unsigned) EOF ) {
+#else
+    while ( (c = readValue()) != EOF ) {
 #endif
- out_bits_init = init_bits;
- max_ocodes = (1 << GIFBITS) - ((1 << (out_bits_init - 1)) + 3);
- did_clear();
- output(code_clear);
- rl_count = 0;
- while (1)
-  { c = readValue();
-    if ((rl_count > 0) && (c != rl_pixel)) rl_flush();
-    if (c == EOF) break;
-    if (rl_pixel == c)
-     { rl_count ++;
-     }
-    else
-     { rl_pixel = c;
-       rl_count = 1;
-     }
-  }
- output(code_eof);
- output_flush();
+
+        in_count++;
+
+        fcode = (long) (((long) c << maxbits) + ent);
+        i = (((long)c << hshift) ^ ent);    /* xor hashing */
+
+        if ( HashTabOf (i) == fcode ) {
+            ent = CodeTabOf (i);
+            continue;
+        } else if ( (long) HashTabOf (i) < 0 )      /* empty slot */
+            goto nomatch;
+        disp = hsize_reg - i;           /* secondary hash (after G. Knott) */
+        if ( i == 0 )
+            disp = 1;
+probe:
+        if ( (i -= disp) < 0 )
+            i += hsize_reg;
+
+        if ( HashTabOf(i) == fcode ) {
+            ent = CodeTabOf (i);
+            continue;
+        }
+        if ( (long) HashTabOf(i) > 0 )
+            goto probe;
+nomatch:
+        output ( (long) ent );
+        out_count++;
+        ent = c;
+#ifdef SIGNED_COMPARE_SLOW
+        if ( (unsigned) free_ent < (unsigned) maxmaxcode) {
+#else
+        if ( free_ent < maxmaxcode ) {
+#endif
+            CodeTabOf (i) = free_ent++; /* code -> hashtable */
+            HashTabOf (i) = fcode;
+        } else
+                cl_block();
+    }
+    /*
+     * Put out the final code.
+     */
+    output( (long)ent );
+    out_count++;
+    output( (long) EOFCode );
+
+    return;
 }
 
-/*-----------------------------------------------------------------------
+/*****************************************************************
+ * TAG( output )
  *
- * End of miGIF section  - See copyright notice at start of section.
+ * Output the given code.
+ * Inputs:
+ *      code:   A n_bits-bit integer.  If == -1, then EOF.  This assumes
+ *              that n_bits =< (long) wordsize - 1.
+ * Outputs:
+ *      Outputs code to the file.
+ * Assumptions:
+ *      Chars are 8 bits long.
+ * Algorithm:
+ *      Maintain a GIFBITS character long buffer (so that 8 codes will
+ * fit in it exactly).  Use the VAX insv instruction to insert each
+ * code in turn.  When the buffer fills up empty it and start over.
+ */
+
+static unsigned long cur_accum = 0;
+static int  cur_bits = 0;
+
+static
+unsigned long masks[] = { 0x0000, 0x0001, 0x0003, 0x0007, 0x000F,
+                                  0x001F, 0x003F, 0x007F, 0x00FF,
+                                  0x01FF, 0x03FF, 0x07FF, 0x0FFF,
+                                  0x1FFF, 0x3FFF, 0x7FFF, 0xFFFF };
+
+static void
+output(code)
+    long  code;
+{
+    cur_accum &= masks[cur_bits];
+
+    if (cur_bits > 0) {
+	cur_accum |= ((long) code << cur_bits);
+    } else {
+	cur_accum = code;
+    }
+
+    cur_bits += n_bits;
+
+    while (cur_bits >= 8 ) {
+	char_out((unsigned int)(cur_accum & 0xff));
+	cur_accum >>= 8;
+	cur_bits -= 8;
+    }
+
+    /*
+     * If the next entry is going to be too big for the code size,
+     * then increase it, if possible.
+     */
+
+    if ((free_ent > maxcode)|| clear_flg ) {
+	if (clear_flg) {
+	    maxcode = MAXCODE(n_bits = g_init_bits);
+	    clear_flg = 0;
+	} else {
+	    n_bits++;
+	    if (n_bits == maxbits) {
+		maxcode = maxmaxcode;
+	    } else {
+		maxcode = MAXCODE(n_bits);
+	    }
+	}
+    }
+
+    if (code == EOFCode) {
+	/*
+	 * At EOF, write the rest of the buffer.
+	 */
+        while (cur_bits > 0) {
+	    char_out((unsigned int)(cur_accum & 0xff));
+	    cur_accum >>= 8;
+	    cur_bits -= 8;
+	}
+	flush_char();
+    }
+}
+
+/*
+ * Clear out the hash table
+ */
+static void
+cl_block()             /* table clear for block compress */
+{
+
+        cl_hash ( (int) hsize );
+        free_ent = ClearCode + 2;
+        clear_flg = 1;
+
+        output((long) ClearCode);
+}
+
+static void
+cl_hash(hsize)          /* reset code table */
+    int hsize;
+{
+    register int *htab_p = htab+hsize;
+    register long i;
+    register long m1 = -1;
+
+    i = hsize - 16;
+    do {                            /* might use Sys V memset(3) here */
+	*(htab_p-16) = m1;
+	*(htab_p-15) = m1;
+	*(htab_p-14) = m1;
+	*(htab_p-13) = m1;
+	*(htab_p-12) = m1;
+	*(htab_p-11) = m1;
+	*(htab_p-10) = m1;
+	*(htab_p-9) = m1;
+	*(htab_p-8) = m1;
+	*(htab_p-7) = m1;
+	*(htab_p-6) = m1;
+	*(htab_p-5) = m1;
+	*(htab_p-4) = m1;
+	*(htab_p-3) = m1;
+	*(htab_p-2) = m1;
+	*(htab_p-1) = m1;
+	htab_p -= 16;
+    } while ((i -= 16) >= 0);
+
+    for (i += 16; i > 0; i--) {
+	*--htab_p = m1;
+    }
+}
+
+
+/******************************************************************************
  *
- *-----------------------------------------------------------------------*/
+ * GIF Specific routines
+ *
+ ******************************************************************************/
+
+/*
+ * Number of characters so far in this 'packet'
+ */
+static int a_count;
+
+/*
+ * Set up the 'byte output' routine
+ */
+static void
+char_init()
+{
+    a_count = 0;
+    cur_accum = 0;
+    cur_bits = 0;
+}
+
+/*
+ * Define the storage for the packet accumulator
+ */
+static unsigned char accum[256];
+
+/*
+ * Add a character to the end of the current packet, and if it is 254
+ * characters, flush the packet to disk.
+ */
+static void
+char_out( c )
+    int c;
+{
+    accum[a_count++] = c;
+    if (a_count >= 254) {
+	flush_char();
+    }
+}
+
+/*
+ * Flush the packet to disk, and reset the accumulator
+ */
+static void
+flush_char()
+{
+    unsigned char c;
+    if (a_count > 0) {
+	c = a_count;
+	tkimg_Write(g_outfile, (CONST char *) &c, 1);
+	tkimg_Write(g_outfile, (CONST char *) accum, a_count);
+	a_count = 0;
+    }
+}
+
+/* The End */
