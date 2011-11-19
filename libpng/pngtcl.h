bfded0ac07a5e789cbe5e8c73eab4376e7d9ed62
nijtmans <nijtmans@8c8d7abf-59c3-4658-88e4-7cea5a6ab77c>
2011-11-19 16:47:13 +0000

    Correct png version number in pngtcl.h
    git-svn-id: https://svn.code.sf.net/p/tkimg/code/trunk@330 8c8d7abf-59c3-4658-88e4-7cea5a6ab77c

--- libpng/pngtcl.h
+++ libpng/pngtcl.h
@@ -26,10 +26,10 @@
 #define PNGTCL_MAJOR_VERSION	1
 #define PNGTCL_MINOR_VERSION	4
 #define PNGTCL_RELEASE_LEVEL	TCL_RELEASE
-#define PNGTCL_RELEASE_SERIAL	3
+#define PNGTCL_RELEASE_SERIAL	8
 
-#define PNGTCL_VERSION		"1.4.3"
-#define PNGTCL_PATCH_LEVEL	"1.4.3"
+#define PNGTCL_VERSION		"1.4.8"
+#define PNGTCL_PATCH_LEVEL	"1.4.8"
 
 /*
  * Used to block the rest of this header file from resource compilers so
