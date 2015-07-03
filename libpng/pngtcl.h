85623c59fb545efe8cb849bca42751778e46010c
nijtmans <nijtmans@8c8d7abf-59c3-4658-88e4-7cea5a6ab77c>
2015-07-03 10:23:25 +0000

    1.4.3 -> 1.4.4
    git-svn-id: https://svn.code.sf.net/p/tkimg/code/trunk@384 8c8d7abf-59c3-4658-88e4-7cea5a6ab77c

--- libpng/pngtcl.h
+++ libpng/pngtcl.h
@@ -28,8 +28,8 @@
 #define PNGTCL_RELEASE_LEVEL	TCL_RELEASE
 #define PNGTCL_RELEASE_SERIAL	8
 
-#define PNGTCL_VERSION		"1.4.15"
-#define PNGTCL_PATCH_LEVEL	"1.4.15"
+#define PNGTCL_VERSION		"1.6.17"
+#define PNGTCL_PATCH_LEVEL	"1.6.17"
 
 /*
  * Used to block the rest of this header file from resource compilers so
